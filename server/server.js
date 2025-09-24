d39a094d9f9cdcca010243a20ba8f54acf2070f7
Chinmayee Mohanty <chinmayeemohanty947@gmail.com>
2025-09-24 21:55:45 +0530

    live url of frontend added

--- server/server.js
+++ server/server.js
@@ -10,7 +10,9 @@ dotenv.config();
 const port = process.env.PORT || 4000;
 
 //allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173,https://greenbasket-chi.vercel.app",
+];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
